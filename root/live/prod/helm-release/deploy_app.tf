3c95ba973b42f80143b0522c640962e541a286f2
noaeisnkut <noaeisnkut@gmail.com>
2025-11-02 20:10:28 +0200

    modified the helm-realeses

--- root/live/prod/helm-release/deploy_app.tf
+++ root/live/prod/helm-release/deploy_app.tf
@@ -1,6 +1,6 @@
 resource "helm_release" "prod_flask_app" {
   name       = "prod-flask-app"
-  namespace  = kubernetes_namespace.prod_ns.metadata[0].name
+  namespace  = "prod"
   repository = "https://github.com/noaeisnkut/second_clothes_project.git"
   chart      = "helm-chart"  
   values     = ["https://raw.githubusercontent.com/noaeisnkut/second_clothes_project/s3/helm-chart/values-prod.yaml"]
