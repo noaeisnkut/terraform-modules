b57bb6cdbabd2280e84e19a2c4685d0aaa361012
noaeisnkut <noaeisnkut@gmail.com>
2026-03-02 17:56:58 +0200

    added k8s provider for installing the argocd in the cluster

--- azure/argocd/providers.tf
+++ azure/argocd/providers.tf
@@ -29,8 +29,8 @@ provider "azurerm" {
   features {}
 }
 provider "kubernetes" {
-  host                   = dependency.aks.outputs.kube_host
-  client_certificate     = base64decode(dependency.aks.outputs.kube_client_certificate)
-  client_key             = base64decode(dependency.aks.outputs.kube_client_key)
-  cluster_ca_certificate = base64decode(dependency.aks.outputs.kube_cluster_ca_certificate)
+  host                   = var.cluster_endpoint
+  client_certificate     = base64decode(var.client_certificate)
+  client_key             = base64decode(var.client_key)
+  cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
 }
