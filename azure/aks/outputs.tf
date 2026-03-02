5d8ef7da8951d5ca4ea51b2e3f5ddfc6789cac06
noaeisnkut <noaeisnkut@gmail.com>
2026-03-02 16:08:52 +0200

    added to aks sensitive tabs to sensitive data output

--- azure/aks/outputs.tf
+++ azure/aks/outputs.tf
@@ -4,24 +4,24 @@ output "oidc_issuer_url" {
 
 output "kube_host" {
   description = "API server host"
-  value       = azurerm_kubernetes_cluster.aks.kube_config.0.host
+  value       = sensitive(azurerm_kubernetes_cluster.aks.kube_config.0.host)
 }
 
 output "kube_client_certificate" {
   description = "Client certificate"
-  value       = azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate
+  value       = sensitive(azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)
   sensitive   = true
 }
 
 output "kube_client_key" {
   description = "Client key"
-  value       = azurerm_kubernetes_cluster.aks.kube_config.0.client_key
+  value       = sensitive(azurerm_kubernetes_cluster.aks.kube_config.0.client_key)
   sensitive   = true
 }
 
 output "kube_cluster_ca_certificate" {
   description = "Cluster CA certificate"
-  value       = azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate
+  value       = sensitive(azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate)
   sensitive   = true
 }
 
