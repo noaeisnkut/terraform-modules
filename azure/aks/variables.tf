d38618e4067b4891dcec461c6ba8f2fc6adbba38
noaeisnkut <noaeisnkut@gmail.com>
2026-03-02 16:54:39 +0200

    assign service_cidr to aks module

--- azure/aks/variables.tf
+++ azure/aks/variables.tf
@@ -62,7 +62,7 @@ variable "pod_cidr" {
 
 variable "service_cidr" {
   type        = string
-  default     = "10.240.0.0/16"
+  default     = "10.0.1.0/27"
   description = "Optional service CIDR"
 }
 
