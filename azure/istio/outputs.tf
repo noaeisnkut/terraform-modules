8673237e784c884a17aa8980dcca6c774bfcc5d2
noaeisnkut <noaeisnkut@gmail.com>
2026-03-03 18:38:11 +0200

    fix istio descovery resource

--- azure/istio/outputs.tf
+++ azure/istio/outputs.tf
@@ -1,8 +1,9 @@
 output "istio_version" {
   value = var.istio_release_version
 }
-
 output "external_gateway_ip" {
-  value       = var.istio_enable_external_gateway ? data.kubernetes_service_v1.external_gw[0].status[0].load_balancer[0].ingress[0].ip : null
+  value = (
+    var.istio_enable_external_gateway && var.external_gateway_service_type == "LoadBalancer"
+  ) ? try(data.kubernetes_service_v1.external_gw[0].status[0].load_balancer[0].ingress[0].ip, null) : null
   description = "Public IP of the external Istio Gateway"
 }
