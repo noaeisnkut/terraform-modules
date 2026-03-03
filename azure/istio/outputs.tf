0afc61757318e51f720d86f206f65e815729d8e9
noaeisnkut <noaeisnkut@gmail.com>
2026-03-03 19:18:24 +0200

    alb-routes added to alb-controller

--- azure/istio/outputs.tf
+++ azure/istio/outputs.tf
@@ -1,9 +1,12 @@
 output "istio_version" {
   value = var.istio_release_version
 }
-output "external_gateway_ip" {
-  value = (
-    var.istio_enable_external_gateway && var.external_gateway_service_type == "LoadBalancer"
-  ) ? try(data.kubernetes_service_v1.external_gw[0].status[0].load_balancer[0].ingress[0].ip, null) : null
-  description = "Public IP of the external Istio Gateway"
+output "istio_namespace" {
+  description = "Namespace where Istio ingress gateway service is deployed"
+  value       = var.istio_release_namespace
+}
+
+output "istio_ingress_service_name" {
+  description = "Service name of the Istio ingress gateway to be used by ALB"
+  value       = "istio-ingressgateway-external"  # the ClusterIP service you created
 }
