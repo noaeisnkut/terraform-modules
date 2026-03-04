c7870b223cb5d8fb9ce40797867d60db7e521057
noaeisnkut <noaeisnkut@gmail.com>
2026-03-04 13:02:23 +0200

    chnaged so svc name and namespace will be outputed for other modules

--- azure/argocd/outputs.tf
+++ azure/argocd/outputs.tf
@@ -1,6 +1,7 @@
 output "argocd" {
   value = {
     username = "admin"
+    namespace = "argocd"
     password = data.kubernetes_secret_v1.argocd-secret.data.password
     svc_name = "argocd-server"  
     svc_port = 80
